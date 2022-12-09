747284ec767a7a82a7992d8370da78bffe225685
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-09 17:54:52 +0530

    Check Frequency of words in a large paragraph

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -5,32 +5,31 @@
         static void Main(string[] args)
         {
             Console.WriteLine("================ WELCOME TO HASHTABLE ========================");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(19);
+            hash.Add(0, "Paranoids");
+            hash.Add(1, "are");
+            hash.Add(2, "not");
+            hash.Add(3, "paranoid");
+            hash.Add(4, "because");
+            hash.Add(5, "they");
+            hash.Add(6, "are");
+            hash.Add(7, "paranoid");
+            hash.Add(8, "but");
+            hash.Add(9, "because");
+            hash.Add(10, "they");
+            hash.Add(11, "keep");
+            hash.Add(12, "putting");
+            hash.Add(13, "themselves");
+            hash.Add(14, "deliberately");
+            hash.Add(15, "into");
+            hash.Add(16, "paranoid");
+            hash.Add(17, "avoidable");
+            hash.Add(18, "situations");
 
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
-
-            string hash0 = hash.Get(0);
-            Console.WriteLine("0th index value: " + hash0);
-
-            string hash1 = hash.Get(1);
-            Console.WriteLine("1st index value: " + hash1);
-
-            string hash2 = hash.Get(2);
-            Console.WriteLine("2nd index value: " + hash2);
-
-            string hash3 = hash.Get(3);
-            Console.WriteLine("3rd index value: " + hash3);
-
-            string hash4 = hash.Get(4);
-            Console.WriteLine("4th index value: " + hash4);
-
-            string hash5 = hash.Get(5);
-            Console.WriteLine("5th index value: " + hash5);
+            for (int i = 0; i < hash.size; i++)
+            {
+                Console.WriteLine($"{i} index value : {hash.Get(i)}");
+            }
 
             Console.WriteLine("\nFrequency of Words ==> ");
 
@@ -41,7 +40,6 @@
             }
 
             hash.FreqOfWords(arr, arr.Length);
-
             Console.ReadLine();
         }
     }
